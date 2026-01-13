3021dd925d74ec4a7fcb03473be4289c3a4f17b7
XuBinX <168260671+XuBinX@users.noreply.github.com>
2026-01-13 15:48:54 +0800

    同事修改了核心输出

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git~");
+        System.out.println("同事修改： hello git_v2~");
         System.out.println("同事新增：拉取测试内容");
     }
 }
